2be2bfcc2ad9a7ad65e74ff369110bac55f1ca73
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2024-03-26 09:56:55 +0100

    sync

--- AutoMapper.cs
+++ AutoMapper.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 
 namespace myRPG
 {
     public class AutoMapper : Profile
-    {
+    
         public AutoMapper()
         {
             CreateMap<Player, GetPlayerDto>();
+            
         }
     }
 }
