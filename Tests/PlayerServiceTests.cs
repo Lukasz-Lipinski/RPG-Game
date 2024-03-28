720e146fd2218cf2ccae217aaa79bb6f88dc8b08
Lipinski <lukasz.lipinski@capgemini.com>
2024-03-28 07:53:29 +0100

    sync

--- Tests/PlayerServiceTests.cs
+++ Tests/PlayerServiceTests.cs
@@ -5,32 +5,29 @@ namespace myRPG.Tests
     public class PlayerServiceTests : IClassFixture<PlayerService>
     {
         private readonly IPlayerService playerService;
-        private Player First_MockPlayer = new()
-        {
-            HP = 100,
-            MP = 100,
-            Level = 1,
-            Name = "First Player",
-            CharacterType = CharacterType.Alive,
-            CharacterClass = CharacterClass.Mag,
-            CharacterRace = CharacterRace.Human
-        };
+        private Player First_MockPlayer =
+            new()
+            {
+                HP = 100,
+                MP = 100,
+                Level = 1,
+                Name = "First Player",
+                CharacterType = CharacterType.Alive,
+                CharacterClass = CharacterClass.Mag,
+                CharacterRace = CharacterRace.Human
+            };
 
-        private Player Second_MockPlayer = new()
-        {
-            HP = 100,
-            MP = 100,
-            Level = 1,
-            Name = "Second Player",
-            CharacterType = CharacterType.Alive,
-            CharacterClass = CharacterClass.Warrior,
-            CharacterRace = CharacterRace.Human
-        };
-
-        public PlayerServiceTests()
-        {
-            this.playerService = new PlayerService();
-        }
+        private Player Second_MockPlayer =
+            new()
+            {
+                HP = 100,
+                MP = 100,
+                Level = 1,
+                Name = "Second Player",
+                CharacterType = CharacterType.Alive,
+                CharacterClass = CharacterClass.Warrior,
+                CharacterRace = CharacterRace.Human
+            };
 
         [Fact]
         public void CheckIfPlayerExistReturnsTrue()
@@ -45,6 +42,5 @@ namespace myRPG.Tests
             Store.Players.Add(this.First_MockPlayer);
             Assert.False(this.playerService.CheckIfPlayerExist(this.Second_MockPlayer));
         }
-
     }
 }
